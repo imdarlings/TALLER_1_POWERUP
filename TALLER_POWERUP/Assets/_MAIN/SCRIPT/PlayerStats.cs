dda9428d82cde5ee592d51175dba14b560ef1b1e
imdarlings <dhinestroza880@soyudemedellin.edu.co>
2026-03-03 17:56:55 -0500

    VARIABLES PRIVADAS - DAÑO - ESCUDO

--- TALLER_POWERUP/Assets/_MAIN/SCRIPT/PlayerStats.cs
+++ TALLER_POWERUP/Assets/_MAIN/SCRIPT/PlayerStats.cs
@@ -2,20 +2,42 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public float maxHealth = 100f;
-    public float currentHealth = 100f;
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth = 100f;
 
-    public float baseSpeed = 5f;
-    public float currentSpeed = 5f;
+    [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float currentSpeed = 5f;
 
-    public bool shieldActive = false;
+    [SerializeField] private bool shieldActive = false;
+
+  
+    public float CurrentHealth => currentHealth;
+    public float MaxHealth => maxHealth;
+    public float CurrentSpeed => currentSpeed;
+    public bool ShieldActive => shieldActive;
+
+
 
     public void Heal(float amount)
     {
+        if (currentHealth >= maxHealth)
+            return;
+
         currentHealth += amount;
 
         if (currentHealth > maxHealth)
             currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (shieldActive)
+            return; // El escudo bloquea el daño
+
+        currentHealth -= amount;
+
+        if (currentHealth < 0)
+            currentHealth = 0;
     }
 
     public void SetSpeedMultiplier(float multiplier)
@@ -23,8 +45,8 @@ public class PlayerStats : MonoBehaviour
         currentSpeed = baseSpeed * multiplier;
     }
 
-    public void SetShield(bool active)
+    public void ToggleShield()
     {
-        shieldActive = active;
+        shieldActive = !shieldActive;
     }
 }
